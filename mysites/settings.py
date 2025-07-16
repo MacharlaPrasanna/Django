eb433cdd72ef2012f89c992cdef84d46ae7ae688
Prasanna <pmacharla@datcloudsolutions.com>
2025-07-16 10:36:22 +0530

    added

--- mysites/settings.py
+++ mysites/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'register',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '3307',
 'OPTIONS': {
             'charset': 'utf8mb4',
